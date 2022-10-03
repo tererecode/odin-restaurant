fad4ef82a64a86d315550a83510b8902b4b86ac8
gustavo <gus09ayala@gmail.com>
2022-10-03 14:28:02 -0300

    Imported loadMenu and attached event listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import loadHome from "./pageModule";
+import loadMenu from "./menuModule";
 
 loadHome();
+
+const navHome = document.getElementById('home');
+const navMenu = document.getElementById('menu');
+const navContact = document.getElementById('contact');
+
+navHome.addEventListener('click', loadHome)
+navMenu.addEventListener('click', loadMenu)
+// navContact.addEventListener('click', handleNavClick)
+
