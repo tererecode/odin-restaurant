6371039d5ecfdc185f3e1fcec3672867cfed8950
gustavo <gus09ayala@gmail.com>
2022-10-03 14:28:30 -0300

    Updated module so it clears content innerHTML

--- src/pageModule.js
+++ src/pageModule.js
@@ -7,6 +7,7 @@ export default function pageLoad() {
     heading.textContent = 'This is some fancy restaurant';
     imgHero.src = restaurantImg;
     copyPara.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero totam necessitatibus provident. Vel neque culpa animi, iste nisi commodi libero enim minus totam non consequatur deleniti? Nesciunt dolore totam ab!'
+    content.innerHTML = ''
     content.appendChild(heading);
     content.appendChild(imgHero);
     content.appendChild(copyPara);
